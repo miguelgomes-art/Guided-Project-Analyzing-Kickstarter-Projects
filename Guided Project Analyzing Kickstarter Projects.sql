72166f6e369a84240521a6b47c8918f1f0878549
Miguel Gomes <miguelgomes647@gmail.com>
2026-05-12 14:49:17 +0100

    Update Guided Project Analyzing Kickstarter Projects.sql

--- Guided Project Analyzing Kickstarter Projects.sql
+++ Guided Project Analyzing Kickstarter Projects.sql
@@ -1,11 +1,5 @@
-/* Introduction
- For this guided project, you'll take on the role of a data analyst at a startup. The product team is considering launching a campaign on Kickstarter to test the viability of some offerings. You've been asked to pull data that will help the team understand what might influence the success of a campaign. The data source is a selection of fields from Kaggle.
-
-Questions to be answered
-
-What types of projects are most likely to be successful?
-Which projects fail?
-
+/*
+Guided Project - Analyzing kickstarter projects
 */
 PRAGMA table_info(ksprojects);
 
